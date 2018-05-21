ef831e8f32acca3e49d1264be21676c8e739443c
eric nicolas <eric.e.nicolas@gmail.com>
2018-05-21 14:30:13 -0700

    webpack intro done

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/,
+          /spec/],
         loader: "eslint-loader"
       }
     ]
